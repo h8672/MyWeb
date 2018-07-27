d1e475d77277ba883f391b4331e50082463b8042
Juha-Matti Kokkonen <juhamk91@gmail.com>
2018-07-27 12:56:15 +0300

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
-"-#-#-#---NewLogEntry---#-#-#-\n\n" >> restart-data.log
+#"-#-#-#---NewLogEntry---#-#-#-\n\n" >> restart-data.log
 echo "Pulling new stuff from git...\n"
 sudo git pull
 echo "\nGit is up-to-date!"
